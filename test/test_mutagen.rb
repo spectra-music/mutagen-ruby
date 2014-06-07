93c40005372df4104578b71d0b0fc56df7f6e020
Katherine Whitlock <toroidalcode@gmail.com>
2014-06-07 18:18:37 -0400

    Add Metadata class
    This commit adds the Metadata class and associated tests.

---
+++ test/test_mutagen.rb
@@ -0,0 +1,27 @@
+require 'minitest_helper'
+
+class TestMetadata < MiniTest::Unit::TestCase
+  class FakeMeta < Mutagen::Metadata
+    def initialize
+    end
+  end
+
+  def test_virtual_constructor
+    assert_raises(NotImplementedError) { Mutagen::Metadata.new 'filename' }
+  end
+
+  def test_load
+    m = Mutagen::Metadata.new
+    assert_raises(NotImplementedError) { m.load 'filename' }
+  end
+
+  def test_virtual_save
+    assert_raises(NotImplementedError) { FakeMeta.new.save }
+    assert_raises(NotImplementedError) { FakeMeta.new.save 'filename' }
+  end
+
+  def test_virtual_delete
+    assert_raises(NotImplementedError) { FakeMeta.new.delete }
+    assert_raises(NotImplementedError) { FakeMeta.new.delete 'filename' }
+  end
+end
